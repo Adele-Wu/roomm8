fefce1a3d3e098075529e1de4c8edbee5856b885
eyun1988 <eyun1@mail.sfsu.edu>
2021-09-18 12:58:37 -0700

    added logout (need css such that cursors is a pointer), fixed Users.js

--- application/public/javascripts/logout.js
+++ application/public/javascripts/logout.js
@@ -1,13 +1,12 @@
 const logout = document.getElementById("logout");
-
+// This is a frontend javascript to fetch to the backend to make a post request to logout.
 logout.addEventListener("click", (e) => {
-    if(logout){
-        fetch('/users/logout', {
-            method: "POST"
-        })
-        .then((data) => {
-            // sessionStorage.close();
-            location.replace('/');
-        })
-    }
+  if (logout) {
+    fetch("/users/logout", {
+      method: "POST",
+    }).then((data) => {
+      // sessionStorage.close();
+      location.replace("/");
+    });
+  }
 });
