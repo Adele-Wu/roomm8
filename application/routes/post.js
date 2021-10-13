580bd0f3b2c8321cb94b7930cb6f73701960a334
Jeffrey <JeffreyFulmerGardner@Outlook.com>
2021-10-12 18:04:42 -0700

    added Browse room filter done

--- application/routes/post.js
+++ application/routes/post.js
@@ -130,5 +130,43 @@ router.get("/:id(\\d+)", async (req, res, next) => {
     res.redirect("/browse-room");
   }
 });
-
+router.get("/filter", async function(request,response,next)
+{
+  let parseObject = Object.fromEntries(
+    Object.entries(request.query).filter(([_, v]) => v != "")
+    );
+  if(Object.keys(parseObject).length===0)
+  {
+    response.redirect('/browse-room')
+  }
+  else
+  {
+    if(parseObject.privacy)
+    {
+      if(parseObject.privacy==="private")
+      {
+        parseObject.privacy=1
+      }
+      else if(parseObject.privacy==="shared")
+      {
+        parseObject.privacy=0
+      }
+    }
+    if(parseObject.minPriceRange || parseObject.maxPriceRange)
+    {
+      if(!parseObject.minPriceRange)
+      {
+        parseObject.minPriceRange = 0.00
+        parseObject.maxPriceRange = parseFloat(parseObject.maxPriceRange)
+      }
+      if(!parseObject.maxPriceRange)
+      {
+        parseObject.maxPriceRange = 900000.00;
+        parseObject.minPriceRange = parseFloat(parseObject.minPriceRange)
+      }
+    }
+    let resultsOfQuery = await Post.filter(parseObject);
+    response.render('browse-room',{results:resultsOfQuery})
+  }
+});
 module.exports = router;
