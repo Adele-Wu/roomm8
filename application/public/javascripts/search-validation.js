98c182b1bb85c8e81a8ec163a95370069ebcc8c1
adele-wu <adelewu2000@gmail.com>
2021-11-06 13:47:53 -0700

    clean up. deleted outputcss, postcss, searchjs, editprofilehbs package pages json pages because they were unused. fixed registration validation. fixed the comment route.

--- application/public/javascripts/search-validation.js
+++ application/public/javascripts/search-validation.js
@@ -19,7 +19,7 @@
 let checkError = true;
 const minAge = document.getElementById("minAgeRange");
 const maxAge = document.getElementById("maxAgeRange");
-const ageRange = document.getElementById("ageRange");
+// const ageRange = document.getElementById("ageRange");
 const filterButton = document.getElementById("filterButton");
 
 function setErrorFor(input, message) {
@@ -38,13 +38,13 @@ function setSuccessFor(input) {
     form_control.className = "form-validation success";
 }
 
-ageRange.addEventListener("input", (e) => {
-    if (parseInt(maxAge.value) < parseInt(minAge.value)) {
-        setErrorFor(minAge, "min value can't be larger than max");
+// ageRange.addEventListener("input", (e) => {
+//     if (parseInt(maxAge.value) < parseInt(minAge.value)) {
+//         setErrorFor(minAge, "min value can't be larger than max");
         
-        filterButton.disabled = true;
-    } else {
-        setSuccessFor(maxAge);
-        filterButton.disabled = false;
-    }
-});
+//         filterButton.disabled = true;
+//     } else {
+//         setSuccessFor(maxAge);
+//         filterButton.disabled = false;
+//     }
+// });
